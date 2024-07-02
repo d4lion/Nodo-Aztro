3200d114d62ea68d24d85a7575b9c9ca140117be
Dann Hoist <111100025+daliondev@users.noreply.github.com>
2024-07-02 16:11:39 -0500

    :bug: fix(NaN Error): When send numbers into input.

--- prueba_5/logic/rectangulo.js
+++ prueba_5/logic/rectangulo.js
@@ -5,18 +5,21 @@ const inputPerimetro = document.getElementById('perimetro')
 const inputDiagonal = document.getElementById('diagonal')
 const btnCalcular = document.getElementById('btnCalcular')
 
-btnCalcular.addEventListener('click', (e) => {
-  e.preventDefault()
+// tomar el evento del formulario "Se hace con document directo para evitar usar mas constantes"
+document
+  .getElementById('formulario-rectangulo')
+  .addEventListener('submit', (e) => {
+    e.preventDefault()
 
-  let base = parseFloat(inputBase.value)
-  let altura = parseFloat(inputAltura.value)
+    let base = parseFloat(inputBase.value)
+    let altura = parseFloat(inputAltura.value)
 
-  let Area = base * altura
-  inputArea.value = `${Area} m²`
+    let Area = base * altura
+    inputArea.value = `${Area.toFixed(2)} m²`
 
-  let Perimetro = base * 2 + altura * 2
-  inputPerimetro.value = `${Perimetro} m²`
+    let Perimetro = base * 2 + altura * 2
+    inputPerimetro.value = `${Perimetro.toFixed(2)} m²`
 
-  let Diagonal = Math.sqrt(Math.pow(base, 2) + Math.pow(altura, 2)).toFixed(2)
-  inputDiagonal.value = `${Diagonal} m²`
-})
+    let Diagonal = Math.sqrt(Math.pow(base, 2) + Math.pow(altura, 2))
+    inputDiagonal.value = `${Diagonal.toFixed(2)} m²`
+  })
