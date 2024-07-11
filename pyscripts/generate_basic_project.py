5a3953ae15a0035a6a551b4857f68c1ee2afab79
d4lion <d4lion.dev@gmail.com>
2024-07-11 16:55:10 -0500

    :sparkles: feat(python-script): Auto generate project

---
+++ pyscripts/generate_basic_project.py
@@ -0,0 +1,17 @@
+"""
+- Recibir entrada por consola
+- Crear archivos segun tipos
+- Recibir nombre del directorio padre
+- Poder crear sub directorios (prueba_x/reto_1)
+- Poder crear archivos adicionales
+"""
+from sys import argv
+
+def crear_archivos(nombre_archivos: str, path: str = )
+
+def main() -> None:
+    args: list[str] = argv
+
+
+if __name__ == '__main__':
+    main()
