a76b7a454d83c94f425bc34fced210e0960a7216
d4lion <d4lion.dev@gmail.com>
2024-07-11 16:57:06 -0500

    :sparkles: feat(python-script): Automatic generate basic project template

---
+++ pyscripts/configs.py
@@ -0,0 +1,1 @@
+ROOT_PATH: str = 'C:/dev/web/Nodo_Aztro_Daniel_Martinez_Tamayo/'
--- pyscripts/generate_basic_project.py
+++ pyscripts/generate_basic_project.py
@@ -6,12 +6,64 @@
 - Poder crear archivos adicionales
 """
 from sys import argv
+from configs import ROOT_PATH
+from os import listdir, mkdir
 
-def crear_archivos(nombre_archivos: str, path: str = )
+
+def create_files(file_name: str, directory: str) -> str:
+    """
+    :param file_name:
+    :param directory:
+    :return: File Path
+    """
+
+    working_path: str = f'{ROOT_PATH}/{directory}'
+
+    # Si el archivo existe no se crea o sobre escribe
+    try:
+        mkdir(working_path)
+    except FileExistsError:
+        pass
+
+    # Si el archivo existe se para la ejecucion
+    if file_name in listdir(working_path):
+        print(f'El archivo {file_name} ya existe')
+        return None
+
+
+    # Se crea el archivo que es solicitado
+    try:
+        with open(f"{working_path}/{file_name}", 'w') as file:
+            print(f'El archivo {file_name} fue creado')
+            file.close()
+    except FileNotFoundError:
+        print(f'Erro al crear el archivo {file_name}')
+
+    return f'{working_path}/{file_name}'
+
 
 def main() -> None:
-    args: list[str] = argv
+    args: list[str] = list(map(lambda arg: arg.lower(), argv))
+
+    if '-h' in args or '--help' in args:
+        print('ARGUMENTOS \n'
+              '1. [plantilla - Basic or None] \n'
+              '2. [directorio - docs/files] \n'
+              '3. [Archivos adicionales - main.js]\n')
+
+    if 'basic' in args:
+        directory: str = args[2]
+        archivos_por_crear: list[str] = ['index.html', 'main.js', 'styles.css', 'README.md']
+
+        if len(args) > 3:
+            for index_file_name in range(3, len(args)):
+                archivos_por_crear.append(args[index_file_name])
+
+        for archivo in archivos_por_crear:
+            create_files(archivo, directory)
 
 
 if __name__ == '__main__':
     main()
+
+
